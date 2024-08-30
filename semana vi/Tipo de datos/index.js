9171c892385a9e3752e0a60f3accd159c9361cdb
isabella castaño serna <isabella.castano758@asys.edu.co>
2024-08-29 20:42:49 -0500

    agregue el contenido visto en clase con mi nueva rama

---
+++ semana vi/Tipo de datos/index.js
@@ -0,0 +1,11 @@
+const esUnString = "Esto es una string";
+console.log(esUnString);
+console.log(typeof esUnString);
+
+const esUnNumero = 123;
+console.log(esUnNumero);
+console.log(typeof esUnNumero);
+
+const esUnBoolean = true;
+console.log(esUnBoolean);
+console.log(typeof esUnBoolean);
